1703d880a82a892da43efaa2277d1e3fd0751034
jabedalamakash <jabedalamakash00@gmail.com>
2023-09-12 12:58:13 +0600

    local stotage doesnt work for uuid issue

--- app/components/Submit.js
+++ app/components/Submit.js
@@ -34,17 +34,17 @@ const Submit = () => {
     }
 
     // get item
-    useEffect(()=>{
-        const data=localStorage.getItem('issues');
-        setstoreData(JSON.parse(data))
+    // useEffect(()=>{
+    //     const data=localStorage.getItem('issues');
+    //     setstoreData(JSON.parse(data))
 
-     },[])
+    //  },[])
 
-     // localStorage setItem
-     useEffect(()=>{
-        localStorage.setItem('issues', JSON.stringify(storeData));
+    //  // localStorage setItem
+    //  useEffect(()=>{
+    //     localStorage.setItem('issues', JSON.stringify(storeData));
 
-     },[storeData])
+    //  },[storeData])
 
 
 
